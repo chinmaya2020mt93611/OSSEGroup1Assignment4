65e561d56d22c8ac15f59a6bd177a6966caeddfe
2020mt93580-pilani <81347347+2020mt93580-pilani@users.noreply.github.com>
2021-11-01 11:53:05 -0700

    updated  numer.java file
    Added additional integer

--- number.java
+++ number.java
@@ -2,11 +2,11 @@ public class SumOfNumbers1
 {  
 public static void main(String args[])   
 {  
-int n1 = 225, n2 = 115, n3=125,n4=100 sum;  
+int n1 = 225, n2 = 115, n3=125,n4=100, n5= 100 sum;  
 
  
 
-sum = n1 + n2 + n3 + n4;  
+sum = n1 + n2 + n3 + n4  + n5 ;  
 
 System.out.println("The sum of numbers is: "+sum);  
 }  

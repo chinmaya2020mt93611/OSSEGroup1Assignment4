2bc1704517cb73c53b5d362f8ceb578d743f89fe
Praveen S <praveen221187@gmail.com>
2021-11-05 17:54:08 +0530

    Commit for changes in main file pull request 2

--- src/main/cpad/Main.java
+++ src/main/cpad/Main.java
@@ -3,6 +3,8 @@ package main.cpad;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	 System.out.println("Pull request 2");
+
+        System.out.println("Pull request 2 commit");
     }
 }
